6ddc339c871a1b47c0ea8d5fcc579313659a7bb5
DaviNakamuraCardoso <davi.nakamura01@gmail.com>
2021-04-16 22:14:08 -0300

    Finishing Project 5

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -6,9 +6,116 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+// while (1){
+//    input = keyboard[];
+//
+//    if (input > 0)
+//    {
+//      color = -1; (black)
+//
+//    }
+//    else {
+//      color = 0; (white)
+//   }
+//
+//    rows = 16
+//    cols = 32
+//    counter = 0
+//    for (i = 0; i < rows; i++) {
+//        for (j = 0; i < cols; j++) {
+//          Screen[counter] = color;
+//          counter += j + i
+//      }
+//   }
+//}
+//
+
+    (LOOP)                      // while (1)
+
+    @KBD                        // input = key
+    D=M
+
+    @input
+    M=D
+
+    @SCREEN                     // counter = screen[0]
+    D=A
+
+    @counter
+    M=D
+
+    @input
+    D=M
+
+    @IF
+    M=0
+
+    @IF                           // if input > 0
+    D;JEQ
+    @ELSE
+    0;JMP
+
+    (IF)
+    @color                        // color = 0 (white)
+    M=0
+    @ENDIF
+    0;JMP
+
+    (ELSE)                        // color = -1 (black)
+    @color
+    M=-1
+
+    @ENDIF
+    0;JMP
+
+    (ENDIF)
+
+    @i                             // i = 0
+    M=0
+
+    @8160                           // rows = 8160
+    D=A
+
+    @rows
+    M=D
+
+    @ENDFOR
+    M=1
+
+    (FOR)   // if i == rows end loop
+
+    @i
+    D=M
+
+    @rows
+    D=D-M
+
+    @ENDFOR
+    D;JEQ
+
+
+    @color
+    D=M
+
+    @counter
+    A=M
+    M=D
+
+    @i      // i++
+    M=M+1
+
+    @counter
+    M=M+1
+
+    @FOR
+    0;JMP
+    (ENDFOR)
+
+    @LOOP   // END
+    0;JMP
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -8,26 +8,31 @@
 //
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
+// r2 = 0;
 // product = r1;
-// n = r2
+// n = r0
 // for (i = 0; i < n; i++) {
-// product = product + product;
+// product = product + r1;
 //}
-// r0 = product
+// r2 = product
+
+    @R2     // r2 = 0
+    M=0
+
     @R1     // product = r1
     D=M
 
     @product
     M=D
 
-    @R2     // n = r2
+    @R0     // n = r0
     D=M
 
     @n
     M=D
 
-    @i      // i = 0
-    M=0
+    @i      // i = 1
+    M=1
 
     (LOOP) // for
     @i      // if i - n == 0 goto STOP
@@ -52,10 +57,10 @@
     0;JMP
 
     (STOP)
-    @product        // r0 = product
+    @product        // r2 = product
     D=M
 
-    @R0
+    @R2
     M=D
 
     (END)
