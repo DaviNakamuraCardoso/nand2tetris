e22b2e4c7d9676f30f92bd4ff63a2da00dad2214
DaviNakamuraCardoso <davi.nakamura01@gmail.com>
2021-04-25 23:32:00 -0300

    Handling commands

---
+++ projects/07/translator/src/commands.c
@@ -0,0 +1,23 @@
+/*
+*
+* Commands for the Davi Compiler Collection (DCC)
+*
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <commands.h>
+
+// Returns a push command (in Hack Assembly) based on a memory set and a value
+char* command_push(char* memset, int value)
+{
+
+}
+
+// Returns a pop command based on the value and memory set
+char* command_pop(char* memset, int value)
+{
+    char* buff;
+    buff = malloc(400 * sizeof(char));
+
+
+}
