886b106ba8dc4374e8182744898dc0d1111da354
DaviNakamuraCardoso <davi.nakamura01@gmail.com>
2021-05-06 22:41:58 -0300

    API for the syntax analyzer

---
+++ projects/10/analyzer/include/tokenizer/precomp.h
@@ -0,0 +1,9 @@
+/*
+*       Header files for the precomp module
+*/
+
+char* handle_whitespaces(char* text);
+
+char* handle_inline_comments(char* text);
+
+char* handle_multiple_line_comments(char* text); 
---
+++ projects/10/analyzer/include/tokenizer/tokens.h
@@ -0,0 +1,27 @@
+/**
+*       Tokenizer header for the Davi Compiler Collection (DCC)
+*/
+
+// Max size of the ascii table
+#define SYMBOL_NUM 127
+
+// Trie that holds symbols
+typedef struct _symbol
+{
+    unsigned int exists:1;
+    struct _symbol* next[SYMBOL_NUM];
+
+} SYMBOL;
+
+
+// Creates a new symbol
+SYMBOL* create_symbol(void);
+
+// Adds a symbol to a root
+void add_symbol(SYMBOL* root, char* symbol);
+
+// Releases a Trie
+void release_symbol(SYMBOL** root);
+
+// Search a symbol in root
+unsigned int search_symbol(SYMBOL* root, char* symbol); 
