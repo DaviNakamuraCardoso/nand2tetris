b5fbf9fb50dffcb8ad307eb97f24a7161337810c
DaviNakamuraCardoso <davi.nakamura01@gmail.com>
2021-05-11 22:26:50 -0300

    Hanlding keywords, symbols and string literals

--- projects/10/analyzer/src/tokenizer/cleaner.c
+++ projects/10/analyzer/src/tokenizer/cleaner.c
@@ -35,21 +35,21 @@ void add_keywords_and_symbols(SYMBOL* root, char* filename, char** (*handler) (c
 
 }
 
-char* keyword_handler(char* expression)
-{
 
-}
-
-char* get_string_literal(char* expression)
+char** get_string_literal(char* expression, char* b)
 {
     int i;
-    char *first, *last, *buff;
+    char *first, *last, *buff, **ret;
 
     // Get the first quote
     first = strchr(expression, '"');
 
     // If there are no double quotes, return NULL
-    if (first == NULL) return NULL;
+    if (first == NULL) {
+        buff = NULL;
+        last = expression;
+        goto endbuff;
+    }
 
     // Get the double quote after the first (does not support escape)
     last = strchr(first+1, '"');
@@ -57,7 +57,7 @@ char* get_string_literal(char* expression)
     if (last == NULL)
     {
         printf("Error: unmatched quote in string declaration\n");
-        return NULL;
+        exit(1);
     }
 
     buff = malloc((3+last-first) * sizeof(char));
@@ -68,8 +68,18 @@ char* get_string_literal(char* expression)
     }
 
     buff[i] = '\0';
+    goto endbuff;
 
-    return buff;
+    endbuff:
+    {
+        ret = malloc(3*sizeof(char*));
+
+        ret[0] = last+1;
+        ret[1] = "StringConstant";
+        ret[2] = buff;
+    }
+
+    return (ret);
 }
 
 unsigned int is_number(char c)
@@ -103,30 +113,45 @@ char* get_number_constant(char* expression)
     return (ret);
 }
 
-char** symbol_handler(char* text, char* buff)
+
+char** handle_symbol_or_keyword(char* text, char* buff, char* key)
 {
-    char* r, **ret;
-    r = malloc(7 * sizeof(char));
-    r = "symbol";
+    char **ret;
 
     ret = malloc(3*sizeof(char*));
 
     ret[0] = text+strlen(buff);
-    ret[1] = r;
+    ret[1] = strdup(key);
     ret[2] = buff;
 
     return (ret);
 }
 
+char** symbol_handler(char* text, char* buff)
+{
+    return (handle_symbol_or_keyword(text, buff, "symbol"));
+}
 
-SYMBOL* symbol_manager(void)
+char** keyword_handler(char* text, char* buff)
+{
+    return (handle_symbol_or_keyword(text, buff, "keyword"));
+}
+
+
+SYMBOL* symbol_manager(char* base_dir)
 {
     SYMBOL* s;
 
+    char path_keywords[200], path_symbols[200];
+
+    sprintf(path_symbols, "%s/symbols.csv", base_dir);
+    sprintf(path_keywords, "%s/keywords.csv", base_dir);
+
     s = create_symbol();
 
-    add_keywords_and_symbols(s, "../syntax/symbols.csv", symbol_handler);
+    add_keywords_and_symbols(s, path_symbols, symbol_handler);
+    add_keywords_and_symbols(s, path_keywords, keyword_handler);
+    add_symbol(s, "\"", get_string_literal);
 
     return s;
-
 }
