92ea3727bc6445108f7d964042a9e767f7a03d10
DaviNakamuraCardoso <davi.nakamura01@gmail.com>
2021-05-19 22:05:36 -0300

    Functions to determine the type of a given token

--- projects/10/analyzer/src/tokenizer/parser.c
+++ projects/10/analyzer/src/tokenizer/parser.c
@@ -1,10 +1,14 @@
 /**
-*       Parser module for the DCC Tokenizer
+*      Parser module for the DCC Tokenizer
 *
 */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <tokenizer/tokens.h>
+#include <tokenizer/precomp.h>
+#include <tokenizer/cleaner.h>
 
 void release_result(char** result)
 {
@@ -14,24 +18,117 @@ void release_result(char** result)
     if (result[2] != NULL) free(result[2]);
 
     free(result);
-
     return;
 }
 
-void write_xml(char** result, FILE* xml)
+
+
+void write_xml(char* tagname, char* content, FILE* xml)
 {
     char opentag[200], closetag[200], final[500];
 
-    sprintf(opentag, "<%s>", result[1]);
-    sprintf(closetag, "</%s>", result[1]);
-    sprintf(final, "%s%s%s", opentag, result[2], closetag);
+    sprintf(opentag, "<%s>", tagname);
+    sprintf(closetag, "</%s>", tagname);
+    sprintf(final, "%s%s%s", opentag, content, closetag);
 
     fprintf(xml, "%s\n", final);
 
-    release_result(result);
-
     return;
 }
+
+char* get_tagname(SYMBOL* root, char* token)
+{
+    TOKEN_TYPE t;
+    t = get_token_type(root, token);
+    return get_tag(t);
+}
+
+unsigned int get_source_line(char* buff, char* source)
+{
+    unsigned int i = 0;
+
+    if (*source == '\0') return 0;
+
+    while ((*source) != '\n')
+    {
+        buff[i++] = source++;
+    }
+
+    return 1;
+}
+
+char* get_xml(char* buff)
+{
+    SYMBOL* types_table = new_types_table();
+    char buff[10000] = '\0';
+    FILE* xml = fopen("tokens.xml");
+
+    while (get_source_line(buff, source))
+    {
+        write_xml(get_tagname(types_table, buff), buff, xml);
+    }
+
+    fclose(xml);
+
+    return get_file("tokens.xml");
+}
+
+SYMBOL* new_types_table(void)
+{
+    SYMBOL* root;
+
+    root = new_symbol();
+
+    add_symbols_from_file(root, "./syntax/keywords.csv", KEYWORD);
+    add_symbols_from_file(root, "./syntax/symbols.csv", IMPLEMENTED_SYMBOL);
+
+    return root;
+
+}
+
+TOKEN_TYPE get_type(SYMBOL* types_table, char* token)
+{
+    TOKEN_TYPE type;
+
+    type = string_or_constant(token);
+
+    if (type != -1) {
+        return type;
+    }
+
+    type = get_token_type(types_table, token);
+
+    if (type != -1) {
+        return type;
+    }
+
+    type = check_variable(token);
+
+    return type;
+}
+
+
+char* get_tag(TOKEN_TYPE type)
+{
+    char* tag;
+
+    switch (type) {
+
+        case IMPLEMENTED_SYMBOL: {
+            tag = "symbol"; break; }
+        case KEYWORD: {
+            tag = "keyword"; break; }
+        case STRING_LITERAL: {
+            tag = "StringConstant"; break; }
+        case VARIABLE: {
+            tag = "variable"; break; }
+        case NUMBER_CONSTANT: {
+            tag = "numberConstant"; break; }
+    }
+
+    return strdup(tag);
+}
+
 
 char* tokenize(char* filename)
 {
--- projects/10/analyzer/tests/src/tokenizer/test_parser.c
+++ projects/10/analyzer/tests/src/tokenizer/test_parser.c
@@ -9,44 +9,121 @@
 #include <tokenizer/cleaner.h>
 #include <tokenizer/parser.h>
 
-
-unsigned int test_write_xml(void)
+unsigned int test_types_table(void)
 {
+    unsigned short s, size;
+    SYMBOL* types_table;
 
-    int size, i;
-    char *file_content;
-    FILE *test_file;
+    char* compare[] = {
+        "while",
+        "if",
+        "&",
+        "{",
+        "hi"
+    };
 
-    test_file = fopen("files/tokenizer/test_write_xml.xml", "w");
+    TOKEN_TYPE types[] = {
+        KEYWORD, KEYWORD, IMPLEMENTED_SYMBOL, IMPLEMENTED_SYMBOL, -1
+    };
 
-    char** results[] = {
-        // get_string_literal("\"Hello, World!\"", "\""),
-        // get_number_constant("3923 Hello", "3"),
+    size = 5;
+
+    types_table = new_types_table();
+
+    for (s = 0; s < size; s++)
+    {
+        if (get_token_type(types_table, compare[s]) != types[s])
+        {
+            printf("Error in types_table, comparison %i\n", s);
+            printf("Result: %i\n", get_token_type(types_table, compare[s]));
+            printf("Expected: %i\n", types[s]);
+            release_symbol(&types_table);
+            return 0;
+        }
+    }
+
+    release_symbol(&types_table);
+    return 1;
+}
+
+unsigned int test_get_type(void)
+{
+    unsigned short s, size;
+
+    char* tokens[] = { "while", "if", "else", "&", "-", "*", "variable",
+    "game", "game_mode", "\"Hello, World\"", "\"Hi\"", "\"Tetris\"","929", "10", "29" };
+
+    TOKEN_TYPE types[] = {
+        KEYWORD, KEYWORD, KEYWORD,
+        IMPLEMENTED_SYMBOL, IMPLEMENTED_SYMBOL, IMPLEMENTED_SYMBOL,
+        VARIABLE, VARIABLE, VARIABLE,
+        STRING_LITERAL, STRING_LITERAL, STRING_LITERAL,
+        NUMBER_CONSTANT, NUMBER_CONSTANT, NUMBER_CONSTANT
 
     };
 
-    size = 2;
+    SYMBOL* root;
 
-    for (i = 0; i < size; i++)
+    root = new_types_table();
+
+    size = 15;
+
+    for (s = 0; s < size; s++)
     {
-        write_xml(results[i], test_file);
+        if (get_type(root, tokens[s]) != types[s])
+        {
+            printf("Error in get_type function, test %i\n", s);
+            printf("Result: %i\n", get_type(root, tokens[s]));
+            printf("Expected: %i\n", types[s]);
+            release_symbol(&root);
+            return 0;
+        }
     }
 
-    fclose(test_file);
+    release_symbol(&root);
 
-    char expected[] = "<StringConstant>Hello, World!</StringConstant>\n<integerConstant>3923</integerConstant>\n";
+    return 1;
+}
 
-    file_content = get_file("files/tokenizer/test_write_xml.xml");
+unsigned int test_get_tag(void)
+{
+    unsigned short s, size;
+    char* result;
 
-    if (strcmp(expected, file_content) != 0)
+    TOKEN_TYPE input[] = {
+        STRING_LITERAL,
+        NUMBER_CONSTANT,
+        VARIABLE,
+        KEYWORD,
+        IMPLEMENTED_SYMBOL
+    };
+
+    char* tags[] = {
+        "StringConstant",
+        "numberConstant",
+        "variable",
+        "keyword",
+        "symbol"
+    };
+
+    size = 5;
+    for (s = 0; s < size; s++)
     {
-        printf("Error in write_xml function\n");
-        printf("Result: %s\nExpected: %s\n", file_content, expected);
-        return 0;
+        result = get_tag(input[s]);
+
+        if (strcmp(result, tags[s]) != 0)
+        {
+            printf("Error in get_tag number %i\n", s);
+            printf("Result: %s\n", result);
+            printf("Expected: %s\n", tags[s]);
+            free(result);
+
+            return 0;
+        }
+        
+        free(result);
+
     }
-
-    free(file_content);
-
 
     return 1;
 }
@@ -56,10 +133,12 @@ unsigned int test_parser(void)
     int i, size;
 
     unsigned int (*tests[]) (void) = {
-        test_write_xml
+        test_types_table,
+        test_get_type,
+        test_get_tag
     };
 
-    size = 0;
+    size = 3;
 
     for (i = 0; i < size; i++)
     {
@@ -69,6 +148,5 @@ unsigned int test_parser(void)
             return 0;
         }
     }
-    printf("Running parser tests.\n");
     return 1;
 }
