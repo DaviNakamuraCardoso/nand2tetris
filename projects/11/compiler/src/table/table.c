369aee0d239523f6b7a2a9cfeb9da73a003fc0b6
DaviNakamuraCardoso <davi.nakamura01@gmail.com>
2021-06-07 21:05:07 -0300

    Symbol table structure

--- projects/11/compiler/src/table/table.c
+++ projects/11/compiler/src/table/table.c
@@ -7,43 +7,144 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <table/table.h>
 
-#define HASH_SIZE 20
 
-TABLE* new_table(char* name, char* type_name, TYPE type, KIND kind, short index)
+static TABLE* talloc(void)
 {
-    TABLE* t = NULL;
 
-    t = malloc(sizeof(TABLE));
+    return (TABLE*) malloc(sizeof(TABLE));
+}
 
-    t->name =  (name != NULL) ? strdup(name) : NULL;
-    t->type_name = (type_name != NULL) ? strdup(type_name) : NULL;
-    t->type = type;
-    t->kind = kind;
-    t->index = index;
+static __VARIABLE* varalloc(void)
+{
+    return (__VARIABLE*) malloc(sizeof(__VARIABLE));
+}
+
+TABLE* new_table(void)
+{
+    TABLE* t = talloc();
+
+    t->variables = calloc(sizeof(__VARIABLE*), HASHSIZE);
+
+    for (int i = 0; i < HASHSIZE; i++)
+    {
+        t->variables[i] = NULL;
+    }
+
     t->next = NULL;
 
+    for (int j = 0; j < 4; j++)
+    {
+        t->kind_counter[j] = 0;
+    }
 
     return t;
 }
 
-void release_table(TABLE** table)
-{
-    free((*table)->name);
-    free((*table)->type_name);
-    free((*table));
 
-    *table = NULL;
+__VARIABLE* new_variable(char* name, KIND k, TYPE t)
+{
+    __VARIABLE* v = varalloc();
+    v->name = strdup(name);
+    v->kind = k;
+    v->type = t;
+    v->index = 0;
+    v->next = NULL;
+
+    return v;
+}
+
+
+static void release_variable(__VARIABLE* v)
+{
+
+    __VARIABLE* current, *next;
+
+    if (v == NULL) return;
+
+    for (current = v; current != NULL; current = next)
+    {
+        next = current->next;
+        free(current->name);
+        free(current);
+    }
 
     return;
 }
 
 
-TABLE** get_hash(char* filename)
+/**
+ *
+ * @param t -> table
+ */
+void release_table(TABLE** t)
 {
-    TABLE** hash;
-    hash = malloc(HASH_SIZE * sizeof(TABLE*));
+    for (int i = 0; i < HASHSIZE; i++)
+    {
+        release_variable((*t)->variables[i]);
+    }
 
+    free(*t);
 
+    *t = NULL;
+
+    return;
 }
+
+void add_hash(TABLE* root, __VARIABLE* variable)
+{
+    int index = hash(variable->name);
+    variable->next = root->variables[index];
+    root->variables[index] = variable;
+
+    // Increase the counter for the kind and assign an index for the variable
+    variable->index = root->kind_counter[variable->kind]++;
+
+    return;
+}
+
+__VARIABLE* search_table(TABLE* root, char* name)
+{
+    int index = hash(name);
+    __VARIABLE* current, *next;
+
+
+    for (current = root->variables[index]; current != NULL; current = next)
+    {
+        next = current->next;
+        if (strcmp(name, current->name) == 0)
+        {
+            return current;
+        }
+    }
+
+    return NULL;
+}
+
+
+void add_var(TABLE* root, char* varname, KIND k, TYPE t)
+{
+    __VARIABLE* v = new_variable(varname, k, t);
+    add_hash(root, v);
+    return;
+}
+
+
+/**
+*   Hash function from K&R,
+*   chapter 6.6, page 128
+*/
+static unsigned int hash(char* s)
+{
+    unsigned int hash_val;
+
+    for (hash_val = 0; *s != '\0'; s++)
+    {
+        hash_val = *s + 31 * hash_val;
+    }
+
+    return (hash_val % HASHSIZE);
+}
+
